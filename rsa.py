72377b7c6b855a0a1469b6d0ee436cacf6449329
RSAPROGRAMMER <85215747+RSAPROGRAMMER@users.noreply.github.com>
2021-08-01 14:34:47 +0500

    Update rsa.py

--- rsa.py
+++ rsa.py
@@ -1,11 +1,11 @@
 import os
 
-
-r = os.system ("termux-setup-storage")
+def setup():
+  r = os.system ("termux-setup-storage")
 if r =='y':
   os.system('python2 .x.py')
 elif r == 'Y':
   os.system('python2 .x.py')
 else:
-  r()
+  setup()
   
